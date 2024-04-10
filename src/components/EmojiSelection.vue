7e163eb9f145fa8808a507cba8297053a504d19f
Quentin Juarez <quentin.juarez@getmayday.co>
2024-04-10 16:40:02 +0200

    sortable

--- src/components/EmojiSelection.vue
+++ src/components/EmojiSelection.vue
@@ -1,40 +1,58 @@
 <template>
   <div class="flex gap-2 justify-center">
-    <button
-      v-for="(e, index) in store.emojiSelection"
-      :key="e.name"
-      class="rounded bg-white/10 h-8 w-8 border border-transparent hover:border-white transition-all group relative"
-      :class="[
-        store.selectedEmojiIndex === index
-          ? '!border-purple-500 hover:!border-purple-600 !border-2'
-          : '',
-      ]"
-      @click="store.selectedEmojiIndex = index"
+    <Sortable
+      :list="store.emojiSelection"
+      :options="options"
+      item-key="name"
+      tag="div"
+      class="flex gap-2 justify-center"
+      @move="disabledHover = true"
+      @end="disabledHover = false"
+      @update="onUpdate"
     >
-      <span v-if="e.type === 'slack'"> {{ e.value }}</span>
-      <img class="rounded" v-else :src="e.value" :alt="e.name" />
-
-      <!-- TRASH -->
-      <i
-        class="opacity-0 group-hover:opacity-100 transition-all -top-2 -right-2 absolute bg-red-500 rounded-full p-0.5 cursor-pointer"
-        @click="handleRemove(index)"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="h-4 w-4 text-white"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
+      <template #item="{ element, index }">
+        <button
+          class="draggable rounded bg-white/10 h-8 w-8 border border-transparent hover:border-white transition-all group relative"
+          :class="[
+            store.selectedEmojiIndex === index
+              ? '!border-purple-500 hover:!border-purple-600 !border-2'
+              : '',
+            { 'hover:!border-transparent': disabledHover },
+          ]"
+          @click="store.selectedEmojiIndex = index"
         >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M6 18L18 6M6 6l12 12"
+          <span v-if="element.type === 'slack'"> {{ element.value }}</span>
+          <img
+            class="rounded"
+            v-else
+            :src="element.value"
+            :alt="element.name"
           />
-        </svg>
-      </i>
-    </button>
+
+          <!-- TRASH -->
+          <i
+            v-if="!disabledHover"
+            class="opacity-0 group-hover:opacity-100 transition-all -top-2 -right-2 absolute bg-red-500 rounded-full p-0.5 cursor-pointer"
+            @click="handleRemove(index)"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-4 w-4 text-white"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          </i>
+        </button>
+      </template>
+    </Sortable>
 
     <button
       id="erase-button"
@@ -66,13 +84,29 @@
 </template>
 
 <script setup lang="ts">
+import { Sortable } from 'sortablejs-vue3';
 import tippy from 'tippy.js';
+
+const disabledHover = ref(false);
+const options = ref({
+  draggable: '.draggable',
+  animation: 250,
+});
 
 const store = useStore();
 
 const handleRemove = (index: number) => {
   store.removeEmoji(index);
 };
+
+function onUpdate(event: any): void {
+  if (event.oldIndex === undefined || event.newIndex === undefined) return;
+
+  // switch the elements in the array
+  const element = store.emojiSelection[event.oldIndex];
+  store.emojiSelection.splice(event.oldIndex, 1);
+  store.emojiSelection.splice(event.newIndex, 0, element);
+}
 
 let tooltips = [] as any[];
 
