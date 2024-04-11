b82ce23d15ec240231b47a192e6fba31585a3e6a
Quentin Juarez <quentin.juarez@getmayday.co>
2024-04-11 10:45:21 +0200

    build 9

---
+++ docs/assets/error-4360c668.js
@@ -0,0 +1,1 @@
+import{_ as r,o as e,c}from"./index-6245249b.js";const o={};function n(t,s){return e(),c("div",null,"Error")}const a=r(o,[["render",n]]);export{a as default};
--- docs/assets/error-6e96087c.js
+++
@@ -1,1 +0,0 @@
-import{_ as r,o as e,c}from"./index-b5f598d5.js";const o={};function n(t,s){return e(),c("div",null,"Error")}const a=r(o,[["render",n]]);export{a as default};
