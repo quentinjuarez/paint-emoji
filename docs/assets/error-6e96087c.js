07d1970ced3a2953a1846c02d9e5f79fc6f4e4d4
Quentin Juarez <quentin.juarez@getmayday.co>
2024-04-11 10:35:42 +0200

    update emojis

---
+++ docs/assets/error-6e96087c.js
@@ -0,0 +1,1 @@
+import{_ as r,o as e,c}from"./index-b5f598d5.js";const o={};function n(t,s){return e(),c("div",null,"Error")}const a=r(o,[["render",n]]);export{a as default};
--- docs/assets/error-ee8456c4.js
+++
@@ -1,1 +0,0 @@
-import{_ as r,o as e,c}from"./index-130f8546.js";const o={};function n(t,s){return e(),c("div",null,"Error")}const a=r(o,[["render",n]]);export{a as default};
