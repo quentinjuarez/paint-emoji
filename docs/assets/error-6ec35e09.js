504090792d821ce951bf97cd0b535b9d62beb70a
QUENTIN <quentin.juarez18@gmail.com>
2024-04-14 23:22:25 +0200

    deploy: v0.3.6

--- docs/assets/error-6ec35e09.js
+++
@@ -1,1 +0,0 @@
-import{_ as r,o as e,c}from"./index-bf816736.js";const o={};function n(t,s){return e(),c("div",null,"Error")}const a=r(o,[["render",n]]);export{a as default};
---
+++ docs/assets/error-dc815a7d.js
@@ -0,0 +1,1 @@
+import{_ as r,o as e,c}from"./index-6f1de8d2.js";const o={};function n(t,s){return e(),c("div",null,"Error")}const a=r(o,[["render",n]]);export{a as default};
