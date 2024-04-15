145d8c9ce5b7b9c6cae41fce3416723d79fe70c2
quentinjuarez <quentin.juarez@hotmail.fr>
2024-04-15 16:36:37 +0200

    deploy: v0.4.6

---
+++ docs/assets/error-d753f377.js
@@ -0,0 +1,1 @@
+import{d as o,H as r,g as t,c as n,o as s}from"./index-7b42faa7.js";const _=o({__name:"error",setup(a){const e=r();return t(()=>{e.push("/")}),(c,u)=>(s(),n("div",null,"ok"))}});export{_ as default};
