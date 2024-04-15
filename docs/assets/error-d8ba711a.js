c1980afa0282106adff4c3668810e7b5edd9bd92
quentinjuarez <quentin.juarez@hotmail.fr>
2024-04-15 13:50:51 +0200

    deploy: v0.4.2

---
+++ docs/assets/error-d8ba711a.js
@@ -0,0 +1,1 @@
+import{d as o,G as r,f as t,c as n,o as s}from"./index-a8482016.js";const _=o({__name:"error",setup(a){const e=r();return t(()=>{e.push("/")}),(c,u)=>(s(),n("div",null,"ok"))}});export{_ as default};
