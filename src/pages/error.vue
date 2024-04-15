b92121d1e908445937263c994e6a4728c76142f8
Quentin Juarez <quentin.juarez@getmayday.co>
2024-04-15 10:17:32 +0200

    redirection

--- src/pages/error.vue
+++ src/pages/error.vue
@@ -1,6 +1,11 @@
 <template>
-  <div >Error</div>
+  <div></div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const router = useRouter();
 
+onMounted(() => {
+  router.push('/');
+});
+</script>
