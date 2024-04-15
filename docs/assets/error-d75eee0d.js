bcf050ad60ab9c7d92f6d41bd36ea0851bdb3fcc
quentinjuarez <quentin.juarez@hotmail.fr>
2024-04-15 10:59:21 +0200

    deploy: v0.3.9

--- docs/assets/error-d75eee0d.js
+++
@@ -1,1 +0,0 @@
-import{_ as r,o as e,c}from"./index-ca76f0ae.js";const o={};function n(t,s){return e(),c("div",null,"Error")}const a=r(o,[["render",n]]);export{a as default};
---
+++ docs/assets/error-d7f7c590.js
@@ -0,0 +1,1 @@
+import{d as o,G as r,f as t,c as n,o as s}from"./index-6d5618e0.js";const _=o({__name:"error",setup(a){const e=r();return t(()=>{e.push("/")}),(c,u)=>(s(),n("div",null,"ok"))}});export{_ as default};
