471caeab642954d9f738ab6fb1305acf7db47a76
quentinjuarez <quentin.juarez@hotmail.fr>
2024-04-17 16:24:12 +0200

    fix deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
-yarn version-patch && \
-git add package.json && \
-yarn build && \
-git add docs/ && \
-git commit -m "deploy: v$(node -p "require('./package.json').version")" && \
-git push
+# Run yarn version-patch and update package.json
+yarn version-patch && git add package.json
+
+# Build the project
+yarn build
+
+# Check if the build was successful
+if [ $? -eq 0 ]; then
+  # Build successful, add docs/ and commit changes
+  git add docs/ && \
+  git commit -m "deploy: v$(node -p "require('./package.json').version")" && \
+  git push
+else
+  # Build failed, discard changes on package.json
+  git checkout -- package.json
+  echo "Build failed. Changes on package.json discarded."
+fi
