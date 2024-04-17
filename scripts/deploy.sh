8f49f5e098e75c51c1c351ee6ab34d70f6077ae5
quentinjuarez <quentin.juarez@hotmail.fr>
2024-04-17 15:56:11 +0200

    fix deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,1 +1,8 @@
-yarn version-patch && git add package.json && yarn build && git add docs/ && git commit -m "deploy: v$(node -p "require('./package.json').version") && git push
+#!/bin/bash
+
+yarn version-patch && \
+git add package.json && \
+yarn build && \
+git add docs/ && \
+git commit -m "deploy: v$(node -p "require('./package.json').version")" && \
+git push
